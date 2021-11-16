c5a4e77c89314f0e8b095ca181b1e31bd09806f9
Samelena <lyly-miranda@yandex.ru>
2021-11-16 21:31:54 +0300

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Спасибо за замечательный курс!!!");
+        System.out.println("Можно воспользоваться тем, что можно не делать последнюю домашку?");
+        System.out.println("Заранее, огромное спасибо!!!");
+    }
+
+}
